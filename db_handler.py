b71a7b1811fe4c2eb4e5cbe7375a7a9662cf83c6
Mason-Kolb <166641742+Mason-Kolb@users.noreply.github.com>
2026-04-25 14:55:47 -0400

    Implemented add_item, add_customer, edit_customer, rent_item, waitlist_customer, update_waitlist, return_item, grant_extension, place_in_line, line_length, save_changes, and close_connection

--- db_handler.py
+++ db_handler.py
@@ -20,7 +20,11 @@ def add_item(new_item: Item = None):
     new_item - An Item object containing a new item to be inserted into the DB in the item table.
         new_item and its attributes will never be None.
     """
-    raise NotImplementedError("you must implement this function")
+    
+    # insert a new item into item table with i_item_sk = max + 1
+    cur.execute(""" INSERT INTO item (i_item_sk, i_item_id, i_rec_start_date, i_product_name, i_brand, i_class, i_category, i_manufact, i_current_price, i_num_owned)
+                    VALUES (( SELECT COALESCE(MAX(i_item_sk), 0) + 1 FROM item), ?, ?, ?, ?, NULL, ?, ?, ?, ?) """,
+                        (new_item.item_id, f"{new_item.start_year}-01-01", new_item.product_name, new_item.brand, new_item.category, new_item.manufact, new_item.current_price, new_item.num_owned))
 
 
 def add_customer(new_customer: Customer = None):
@@ -28,7 +32,38 @@ def add_customer(new_customer: Customer = None):
     new_customer - A Customer object containing a new customer to be inserted into the DB in the customer table.
         new_customer and its attributes will never be None.
     """
-    raise NotImplementedError("you must implement this function")
+    
+    # split name into first and last name
+    name_parts = new_customer.name.strip().split(" ", 1)
+    first_name = name_parts[0]
+    last_name = name_parts[1] if len(name_parts) > 1 else ""
+
+    # parsing address
+    street, city, zip = [part.strip() for part in new_customer.address.split(",", 2)]
+
+    # parsing street into number and name
+    street_parts = street.split(" ", 1)
+    street_number = street_parts[0]
+    street_name = street_parts[1] if len(street_parts) > 1 else ""
+
+    # parsing state and zip
+    state_zip_parts = zip.split(" ", 1)
+    state = state_zip_parts[0]
+    zip_code = state_zip_parts[1] if len(state_zip_parts) > 1 else ""
+
+    # insert into customer address table
+    cur.execute(""" INSERT INTO customer_address (ca_address_sk, ca_street_number, ca_street_name, ca_city, ca_state, ca_zip)
+                    VALUES (( SELECT COALESCE(MAX(ca_address_sk), 0) + 1 FROM customer_address), ?, ?, ?, ?, ?) """,
+                        (street_number, street_name, city, state, zip_code))
+    
+    # get address sk just inserted
+    cur.execute("SELECT MAX(ca_address_sk) FROM customer_address")
+    address_sk = cur.fetchone()[0]
+
+    # insert into customer table
+    cur.execute(""" INSERT INTO customer (c_customer_sk, c_customer_id, c_first_name, c_last_name, c_email_address, c_current_addr_sk)
+                    VALUES (( SELECT COALESCE(MAX(c_customer_sk), 0) + 1 FROM customer), ?, ?, ?, ?, ?) """,
+                        (new_customer.customer_id, first_name, last_name, new_customer.email, address_sk))
 
 
 def edit_customer(original_customer_id: str = None, new_customer: Customer = None):
@@ -36,7 +71,43 @@ def edit_customer(original_customer_id: str = None, new_customer: Customer = Non
     original_customer_id - A string containing the customer id for the customer to be edited.
     new_customer - A Customer object containing attributes to update. If an attribute is None, it should not be altered.
     """
-    raise NotImplementedError("you must implement this function")
+    
+    # get address foreign key to update address if needed
+    cur.execute(""" SELECT c_current_addr_sk FROM customer WHERE c_customer_id=? """, (original_customer_id,))
+
+    address_sk = cur.fetchone()[0]
+
+    # sql set clauses
+    updates = []
+    # corresponding params for set clauses, in same order
+    params = []
+
+    # update name if needed
+    if new_customer.name is not None:
+        name_parts = new_customer.name.strip().split(" ", 1)
+        first_name = name_parts[0]
+        last_name = name_parts[1] if len(name_parts) > 1 else ""
+
+        updates.append("c_first_name=?")
+        params.append(first_name)
+
+        updates.append("c_last_name=?")
+        params.append(last_name)
+
+    # update email if needed
+    if new_customer.email is not None:
+        updates.append("c_email_address=?")
+        params.append(new_customer.email)
+    
+    # update customer id if needed
+    if new_customer.customer_id is not None:
+        updates.append("c_customer_id=?")
+        params.append(new_customer.customer_id)
+    
+    # exe only if there is something to update
+    if len(updates) > 0:
+        params.append(original_customer_id)
+        cur.execute(f""" UPDATE customer SET {', '.join(updates)} WHERE c_customer_id=? """, tuple(params))
 
 
 def rent_item(item_id: str = None, customer_id: str = None):
@@ -44,34 +115,66 @@ def rent_item(item_id: str = None, customer_id: str = None):
     item_id - A string containing the Item ID for the item being rented.
     customer_id - A string containing the customer id of the customer renting the item.
     """
-    raise NotImplementedError("you must implement this function")
+
+    today = date.today()
+    due_date = today + timedelta(days=14)
+
+    # insert rental record into rental table
+    cur.execute("""INSERT INTO rental (item_id, customer_id, rental_date, due_date) VALUES (?, ?, ?, ?)""", (item_id, customer_id, today, due_date))
 
 
 def waitlist_customer(item_id: str = None, customer_id: str = None) -> int:
     """
     Returns the customer's new place in line.
     """
-    raise NotImplementedError("you must implement this function")
+
+    # find next place in line
+    new_place = line_length(item_id) + 1
+    
+    # insert into waitlist
+    cur.execute(""" INSERT INTO waitlist (item_id, customer_id, place_in_line) VALUES (?, ?, ?)""", (item_id, customer_id, new_place))
+
+    return new_place
 
 def update_waitlist(item_id: str = None):
     """
     Removes person at position 1 and shifts everyone else down by 1.
     """
-    raise NotImplementedError("you must implement this function")
+    
+    # remove person at front of line
+    cur.execute(""" DELETE FROM waitlist WHERE item_id=? AND place_in_line=1 """, (item_id,))
+
+    # shift everyone else up by 1
+    cur.execute(""" UPDATE waitlist SET place_in_line = place_in_line - 1 WHERE item_id=? """, (item_id,))
 
 
 def return_item(item_id: str = None, customer_id: str = None):
     """
     Moves a rental from rental to rental_history with return_date = today.
     """
-    raise NotImplementedError("you must implement this function")
+    
+    # get rental record
+    cur.execute(""" SELECT item_id, customer_id, rental_date, due_date FROM rental WHERE item_id=? AND customer_id=? """, (item_id, customer_id))
+    
+    rental_info = cur.fetchone()
+
+    if rental_info is None:
+        return
+    
+    # insert into rental_history
+    cur.execute(""" INSERT INTO rental_history (item_id, customer_id, rental_date, due_date, return_date) VALUES (?, ?, ?, ?, ?) """, (rental_info[0], rental_info[1], rental_info[2], rental_info[3], date.today()))
+
+    # delete from rental
+    cur.execute(""" DELETE FROM rental WHERE item_id=? AND customer_id=? """, (item_id, customer_id))
 
 
 def grant_extension(item_id: str = None, customer_id: str = None):
     """
     Adds 14 days to the due_date.
     """
-    raise NotImplementedError("you must implement this function")
+    
+    # update due date by adding 14 days
+    cur.execute(""" UPDATE rental SET due_date = DATE_ADD(due_date, INTERVAL 14 DAY) WHERE item_id=? AND customer_id=? """, (item_id, customer_id))
 
 
 def get_filtered_items(filter_attributes: Item = None,
@@ -144,26 +247,39 @@ def place_in_line(item_id: str = None, customer_id: str = None) -> int:
     """
     Returns the customer's place_in_line, or -1 if not on waitlist.
     """
-    raise NotImplementedError("you must implement this function")
+
+    # query for place in line
+    cur.execute(""" SELECT place_in_line FROM waitlist WHERE item_id=? AND customer_id=? """, (item_id, customer_id))
+    
+    res = cur.fetchone()
+    
+    if res is None:
+        return -1
+    return res[0]
 
 
 def line_length(item_id: str = None) -> int:
     """
     Returns how many people are on the waitlist for this item.
     """
-    raise NotImplementedError("you must implement this function")
+    
+    # counts how many people are on the waitlist for this item
+    cur.execute("SELECT COUNT(*) FROM waitlist WHERE item_id=? ", (item_id,))
+    return cur.fetchone()[0]
 
 
 def save_changes():
     """
     Commits all changes made to the db.
     """
-    raise NotImplementedError("you must implement this function")
+    
+    conn.commit()
 
 
 def close_connection():
     """
     Closes the cursor and connection.
     """
-    raise NotImplementedError("you must implement this function")
 
+    cur.close()
+    conn.close()
